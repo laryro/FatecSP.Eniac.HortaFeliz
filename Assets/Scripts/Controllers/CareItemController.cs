fe903e01bf0a4ba315bbab79bedc4da97a0bc268
Thiago Canto <tcanto88@yahoo.com.br>
2017-06-04 20:36:01 -0300

    + Science game base classes created + Drag n Drop working correctly and Vase is receiving the dropped item + Still need to do scoring stuff and needs rendering

---
+++ Assets/Scripts/Controllers/CareItemController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CareItemController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    Transform positionToReturnTo = null;
+    GameObject draggableShadow = null;
+
+    public void Start()
+    {
+        draggableShadow = gameObject.transform.GetChild(0).gameObject;
+        positionToReturnTo = gameObject.transform;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        this.GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log(positionToReturnTo.position);
+        draggableShadow.transform.position = eventData.position;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.GetComponent<CanvasGroup>().blocksRaycasts = true;
+        draggableShadow.transform.position = positionToReturnTo.position;
+    }
+}
