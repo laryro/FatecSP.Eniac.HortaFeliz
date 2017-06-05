6a985d2abde86c1cdf4675387354aebf0ca8fa9a
Julio Andrade <julio.andrade@voceqpad.com.br>
2017-06-04 22:06:30 -0300

    Ajuste

--- Assets/Scripts/Controllers/PtOneController.cs
+++ Assets/Scripts/Controllers/PtOneController.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
