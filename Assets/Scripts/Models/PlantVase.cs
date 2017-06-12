c1a450efb578324b95e3ebe278efd8bcd96a68b6
Thiago Canto <thiago.canto@18digital.com.br>
2017-06-12 14:47:35 -0300

    Added sounds to the correct cares, complete vase and misses Added feedback as vase is completed

--- Assets/Scripts/Models/PlantVase.cs
+++ Assets/Scripts/Models/PlantVase.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlantVase : PlantVaseController {
     private Seed seed;
@@ -9,10 +10,15 @@ public class PlantVase : PlantVaseController {
     private GameObject score;
     private int errorMod = 1;
 
+	private AudioSource audioPlayer;
+
+	public AudioClip clearAudio, scoreAudio, errorAudio;
     public List<GameObject> needsList;
 
     public new void Start(){
         base.Start();
+		audioPlayer = this.GetComponent<AudioSource>();
+
         needsPanel = gameObject.transform.Find("NeedsList").gameObject;
 
         score = GameObject.Find("ScoreBlock").gameObject;
@@ -41,14 +47,18 @@ public class PlantVase : PlantVaseController {
             // Desconta pontos
             score.GetComponent<ScoreController>().SubtractPoints(errorMod);
             errorMod++;
+			audioPlayer.PlayOneShot(errorAudio);
         }
 
         // Checa se todos as necessidades foram preenchidas, remove e adiciona outro vaso no lugar
-        if ( RemainingNeeds() == 0)
-        {
-            score.GetComponent<ScoreController>().AddPoints(100);
-            RenewVase();
-        }
+		if (RemainingNeeds () == 0) {
+			score.GetComponent<ScoreController> ().AddPoints (100);
+			audioPlayer.PlayOneShot (clearAudio);
+			this.GetComponent<Image> ().color = new Color (1f, 1f, 1f, 0.3f);
+			StartCoroutine(WaitUpAndRenew (2));
+		} else if( rightNeed ) {
+			audioPlayer.PlayOneShot (scoreAudio);
+		}
     }
 
     /**
@@ -82,7 +92,15 @@ public class PlantVase : PlantVaseController {
             Destroy(needsPanel.transform.GetChild(i).gameObject);
         }
 
+		this.GetComponent<Image>().color = new Color(1f,1f,1f,1f);
         seed = new Seed();
         ShowNeeds();
     }
+
+	private IEnumerator WaitUpAndRenew(int time){
+		yield return new WaitForSeconds(time);
+		RenewVase();
+		yield break;
+	}
+		
 }
